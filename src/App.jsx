07b128434010dddb8cbfd8e800fe026480278b27
Jordan Kelly <jkyne6368@yahoo.com>
2024-08-17 14:08:22 -0400

    minor updtes

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import FeatureSection from "./components/FeatureSection";
+import Workflow from "./components/Workflow";
+import Footer from "./components/Footer";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection id="hero" />
+        <FeatureSection id="features" />
+        <Workflow id="workflow" />
+        <Pricing id="pricing" />
+        <Testimonials id="testimonials" />
+        <Footer id="footer" />
+      </div>
+    </>
+  );
+};
+
+export default App;
